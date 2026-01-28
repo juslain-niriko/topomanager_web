4115baff65aa725b825191eb786c4f06e9fa21ed
MichelYves <michelyvesrazafindrakoto@gmail.com>
2026-01-28 15:10:27 +0800

    iny ary

--- resources/js/Pages/dashboard/Dashboard.jsx
+++ resources/js/Pages/dashboard/Dashboard.jsx
@@ -40,7 +40,7 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
-            teste
+            teste gff
          </MainLayout>
     );
 }
