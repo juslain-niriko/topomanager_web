292127ba7a6f15141fadf66f1f1b79e739e3dbaf
juslain-niriko <drakecafrey@gmail.com>
2026-01-29 10:39:53 +0300

    integration template
    pages:
    - Dashboard (page statique)
     - section suivi nbr dossier par agent
     - section statistique des dossiers traiter
     - section rapport d'activité
       - section liste dossier
    - Suivi_dossier

--- resources/js/Pages/dashboard/Dashboard.jsx
+++ resources/js/Pages/dashboard/Dashboard.jsx
@@ -40,7 +40,243 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
-            teste gff jhj
-         </MainLayout>
+            <div className="row row-cols-1 row-cols-md-2 g-4 mt-2 mb-2">
+                <div className="col-12 col-lg-8">
+                    <div className="card h-100">
+                        <div className="card-header">
+                            Suivis des nombres de dossier traiter par agent
+                        </div>
+                        <div className="card-body">
+                            <form id="formId">
+                                <div className="form-group row">
+                                    <div className="col-4">
+                                        <select
+                                            className="form-control form-control-sm"
+                                            id="exampleFormControlSelect2"
+                                            name="type_utilisauter"
+                                        >
+                                            <option>SuperAdmin</option>
+                                            <option>Caissier </option>
+                                            <option>Archiviste</option>
+                                            <option>Opérateur</option>
+                                            <option>CCT </option>
+                                            <option>CSRT </option>
+                                            <option>
+                                                Secrétaire Technique
+                                            </option>
+                                            <option>Agent de Repérage</option>
+                                            <option>Vérificateur</option>
+                                            <option>Agent de suivi</option>
+                                            <option>Integrateur</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        <select
+                                            className="form-control form-control-sm"
+                                            id="exampleFormControlSelect2"
+                                            name="mois"
+                                        >
+                                            <option>JANVIER</option>
+                                            <option>FEVRIER</option>
+                                            <option>MARS</option>
+                                            <option>AVRIL</option>
+                                            <option>MAI</option>
+                                            <option>JUIN</option>
+                                            <option>JUILLET</option>
+                                            <option>AOUT</option>
+                                            <option>SEPTEMBRE</option>
+                                            <option>OCTOBRE</option>
+                                            <option>NOVEMNBRE</option>
+                                            <option>DECEMBRE</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        <input
+                                            type="number"
+                                            name="anne"
+                                            className="form-control form-control-sm"
+                                        />
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <button
+                                            type="submit"
+                                            className="btn btn-outline-primary btn-sm"
+                                            id="recherche"
+                                        >
+                                            Filtrer
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                            <table className="table table-bordered align-middle table-striped table-hover mt-2">
+                                <thead className="table_head">
+                                    <tr>
+                                        <th>
+                                            <i className="fas fa-user me-1"></i>
+                                            Nom et Prénom
+                                        </th>
+                                        <th className="text-center">
+                                            <i className="fas fa-folder-open me-1"></i>
+                                            Nbr/Dossier
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="userTableBody">
+                                    <tr>
+                                        <td>Admin Administrateur</td>
+                                        <td className="text-center">1 </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Chef Naly</td>
+                                        <td className="text-center">0 </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-4">
+                    <div className="card h-100">
+                        <div className="card-header">
+                            Statistique des dossiers traiter
+                        </div>
+                        <div className="card-body">
+                            <div className="chartjs-size-monitor">
+                                <div className="chartjs-size-monitor-expand">
+                                    <div className=""></div>
+                                </div>
+                                <div className="chartjs-size-monitor-shrink">
+                                    <div className=""></div>
+                                </div>
+                            </div>
+                            <canvas
+                                id="pieChart"
+                                width="420"
+                                height="210"
+                                className="chartjs-render-monitor"
+                            ></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    Rapport d'activiter
+                    <div
+                        className=" btn-custom-bar hint hint--bottom"
+                        data-hint="Export rapport d'activiter"
+                    >
+                        <span
+                            className="fa-solid fa-download"
+                            data-bs-toggle="modal"
+                            data-bs-target="#staticBackdrop"
+                        ></span>
+                    </div>
+                </div>
+                <div className="card-body">
+                    <form id="formRapport">
+                        <div className="row form-group">
+                            <div className="col-sm-4">
+                                <select
+                                    id="selectTraitement"
+                                    className="form-control form-control-sm"
+                                    name="idtraitement"
+                                >
+                                    <option value="1">Plan regulier </option>
+                                    <option value="2">
+                                        Projet de morcellement{" "}
+                                    </option>
+                                </select>
+                            </div>
+                            <div className="col-sm-3">
+                                <select
+                                    id="selectMois"
+                                    className="form-control form-control-sm"
+                                    name="mois"
+                                >
+                                    <option>JANVIER</option>
+                                    <option>FEVRIER</option>
+                                    <option>MARS</option>
+                                    <option>AVRIL</option>
+                                    <option>MAI</option>
+                                    <option>JUIN</option>
+                                    <option>JUILLET</option>
+                                    <option>AOUT</option>
+                                    <option>SEPTEMBRE</option>
+                                    <option>OCTOBRE</option>
+                                    <option>NOVEMNBRE</option>
+                                    <option>DECEMBRE</option>
+                                </select>
+                            </div>
+                            <div className="col-sm-3">
+                                <input
+                                    type="number"
+                                    id="inputAnnee"
+                                    name="anne"
+                                    className="form-control form-control-sm"
+                                    min="1900"
+                                    max="3000"
+                                />
+                            </div>
+
+                            <div className="col">
+                                <button
+                                    type="submit"
+                                    className="btn btn-outline-primary btn-sm"
+                                    id="recherche"
+                                >
+                                    Filtrer
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <table className="table table-bordered align-middle table-striped table-hover mt-2">
+                        <thead className="table_head">
+                            <tr>
+                                <th>
+                                    <i className="fas fa-tasks me-1"></i> Activités
+                                </th>
+                                <th className="text-center">
+                                    <i className="fas fa-circle-notch me-1"></i>{" "}
+                                    Situation
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Instance debut du mois</td>
+                                <td className="text-center" id="intance_deb_m">
+                                    5{" "}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Reçue du mois</td>
+                                <td className="text-center" id="entrants_r">
+                                    1
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Travaux réalisés du mois</td>
+                                <td className="text-center" id="realiser">
+                                    0
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Cumul du mois</td>
+                                <td className="text-center" id="interne_r">
+                                    6
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Instance fin du mois</td>
+                                <td className="text-center" id="sortant_r">
+                                    6
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </MainLayout>
     );
 }
--- resources/js/Pages/suivie_dossier/Suivi_dossier.jsx
+++ resources/js/Pages/suivie_dossier/Suivi_dossier.jsx
@@ -2,8 +2,255 @@ import MainLayout from "@/Layouts/MainLayout";
 
 export default function Suivi_dossier() {
     return (
-       <MainLayout>
-
+        <MainLayout>
+            <div className="card">
+                <div className="card-header">
+                    <form
+                        className="row"
+                        id="formSearchAllDossier"
+                        method="post"
+                    >
+                        <div className="col">
+                            <input
+                                type="text"
+                                id="rtx_all_dossier"
+                                className="form-control"
+                                placeholder="NUMERO"
+                            />
+                        </div>
+                        <div className="col-auto">
+                            <label>De</label>
+                        </div>
+                        <div className="col">
+                            <input
+                                type="date"
+                                className="form-control"
+                                id="date_debut"
+                            />
+                        </div>
+                        <div className="col-auto">
+                            <label>À</label>
+                        </div>
+                        <div className="col">
+                            <input
+                                type="date"
+                                className="form-control"
+                                id="date_fin"
+                            />
+                        </div>
+                        <div className="col">
+                            <select
+                                className="form-select"
+                                id="type-traitement"
+                            >
+                                <option value="">Tous traitements</option>
+                                <option value="1">Plan regulier </option>
+                                <option value="2">
+                                    Projet de morcellement{" "}
+                                </option>
+                                <option value="3">
+                                    Rétablissement de borne ou de limite{" "}
+                                </option>
+                            </select>
+                        </div>
+                        <div className="col">
+                            <select className="form-select" id="type-procedure">
+                                <option value="">Tous procedures</option>
+                                <option value="22">Lecture </option>
+                                <option value="26">
+                                    Retour dossier domaine{" "}
+                                </option>
+                                <option value="31">Attribution RTX </option>
+                                <option value="30">Repérage provisoire </option>
+                            </select>
+                        </div>
+                        <div className="col">
+                            <div>
+                                <div className="position-relative">
+                                    <select
+                                        className="select2 select2-hidden-accessible"
+                                        id="dm_search"
+                                        multiple=""
+                                        data-select2-id="dm_search"
+                                        tabIndex="-1"
+                                        aria-hidden="true"
+                                    >
+                                        <option>
+                                            ANDRIANIAINA Fandresena{" "}
+                                        </option>
+                                        <option>RAMIAMARISOA Condocs </option>
+                                        <option>RAFANOMEZANTSOA Swdg </option>
+                                        <option>NRK12 Admin </option>
+                                    </select>
+                                    <span
+                                        className="select2 select2-container select2-container--default"
+                                        dir="ltr"
+                                        data-select2-id="1"
+                                    >
+                                        <span className="selection">
+                                            <span
+                                                className="select2-selection select2-selection--multiple"
+                                                role="combobox"
+                                                aria-haspopup="true"
+                                                aria-expanded="false"
+                                                tabIndex="-1"
+                                                aria-disabled="false"
+                                            >
+                                                <ul className="select2-selection__rendered">
+                                                    <li className="select2-search select2-search--inline">
+                                                        <input
+                                                            className="select2-search__field"
+                                                            type="search"
+                                                            tabIndex="0"
+                                                            autoComplete="off"
+                                                            autoCorrect="off"
+                                                            autoCapitalize="none"
+                                                            spellCheck="false"
+                                                            role="searchbox"
+                                                            aria-autocomplete="list"
+                                                            placeholder="Demandeur"
+                                                        />
+                                                    </li>
+                                                </ul>
+                                            </span>
+                                        </span>
+                                        <span
+                                            className="dropdown-wrapper"
+                                            aria-hidden="true"
+                                        ></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <button type="submit" className="btn btn-primary">
+                                Recherche
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div className="card-body table_custom_1">
+                    <table className="table table-bordered text-center align-middle table-hover">
+                        <thead className="table_head">
+                            <tr>
+                                <th>
+                                    <i className="fas fa-hashtag me-1"></i> N°
+                                    RTX
+                                </th>
+                                <th>
+                                    <i className="fas fa-code-branch me-1"></i>{" "}
+                                    N° Origine
+                                </th>
+                                <th>
+                                    <i className="fas fa-certificate me-1"></i>{" "}
+                                    N° Titre
+                                </th>
+                                <th>
+                                    <i className="fas fa-certificate me-1"></i>{" "}
+                                    Nom de proprieter
+                                </th>
+                                <th>
+                                    <i className="fas fa-briefcase me-1"></i>{" "}
+                                    Nature du traitement
+                                </th>
+                                <th>
+                                    <i className="fa-solid fa-stairs"></i>{" "}
+                                    Procedure actuelle
+                                </th>
+                                <th>
+                                    <i className="fas fa-hourglass-half me-1"></i>{" "}
+                                    Durée (Jours)
+                                </th>
+                                <th>
+                                    <i className="fas fa-user me-1"></i>{" "}
+                                    Demandeur
+                                </th>
+                                <th>
+                                    <i className="fas fa-stopwatch me-1"></i>{" "}
+                                    Temps écoulé (Jours)
+                                </th>
+                                <th>
+                                    <i className="fas fa-calendar-day me-1"></i>{" "}
+                                    Date entrée
+                                </th>
+                                <th>
+                                    <i className="fas fa-circle-notch me-1"></i>{" "}
+                                    État
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="attQryTableDossier">
+                            <tr>
+                                <td className="py-1"></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>Plan regulier</td>
+                                <td>Lecture</td>
+                                <td>22</td>
+                                <td>ANDRIAMANARIVO Ernest </td>
+                                <td>205</td>
+                                <td>2025-07-07</td>
+                                <td>DOSSIER EN RETARD! </td>
+                            </tr>
+                            <tr
+                            // onclick="window.location='http://localhost/demat_topo_/redirect_detail_suivie_dossier/2';"
+                            >
+                                <td className="py-1"></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>Plan regulier</td>
+                                <td>Lecture</td>
+                                <td>22</td>
+                                <td>RAMIAMARISOA Condocs </td>
+                                <td>203</td>
+                                <td>2025-07-09</td>
+                                <td>DOSSIER EN RETARD! </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="card-footer form-inline">
+                    <input
+                        className="btn_custom_footer"
+                        type="button"
+                        label="Première"
+                    />
+                    <input
+                        className="btn_custom_footer"
+                        type="button"
+                        // value="Précédente"
+                    />
+                    <input
+                        className="btn_custom_footer_disable"
+                        type="text"
+                        // value="1"
+                        disabled=""
+                    />
+                    <select
+                        className="btn_custom_footer form-control-sm"
+                        name=""
+                        id=""
+                    >
+                        <option
+                        // value=""
+                        >
+                            1/1
+                        </option>
+                    </select>
+                    <input
+                        className="btn_custom_footer"
+                        type="button"
+                        // value="Suivante"
+                    />
+                    <input
+                        className="btn_custom_footer"
+                        type="button"
+                        // value="Dernière"
+                    />
+                </div>
+            </div>
         </MainLayout>
-    )
+    );
 }
